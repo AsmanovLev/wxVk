465e59aa757cf9efd9ee0d10d5247a010fb57261
Asmanov Lev <asmanovlev@gmail.com>
2022-02-13 20:48:25 +0700

    Possible wrong action: creating class AskWindow

--- main.py
+++ main.py
@@ -11,7 +11,9 @@ locale_en_US_UTF_8 = {
     "password": "Password",
     "gettoken": "Get token",
     "token": "Token",
-    "enter_credentials" : "Enter your credentials"
+    "enter_credentials" : "Enter your credentials",
+    "sumbit" : "Sumbit",
+    '2fa' : "Enter 2FA code"
 }
 locale = {}
 if language == "en_US.UTF-8":
@@ -26,6 +28,34 @@ def auth_handler():
     key = input("Введите код для двухфакторной аутификации: ")
     remember_device = True
     return key, remember_device
+
+
+class AskWindow(wx.Frame):
+    def __init__(self, *args, **kwargs):
+        super(AskWindow, self).__init__(*args, **kwargs)
+        self.InitUI()
+        self.code = ""
+    def InitUI(self):
+        # wx.Frame(None, style=wx.CAPTION | wx.CLIP_CHILDREN)
+        panel = wx.Panel(self, wx.ID_ANY)
+        PasswordText = wx.TextCtrl(panel, -1, "", size=(100, -1))
+        PasswordText.SetInsertionPoint(0)
+        def onButton(frame):
+            self.code = PasswordText.GetLineText(0)
+            
+            self.OnQuit(frame)
+        button = wx.Button(panel, wx.ID_ANY, locale['sumbit'], (10, 10))
+        button.Bind(wx.EVT_BUTTON, onButton)
+        sizer = wx.FlexGridSizer(cols=2, hgap=6, vgap=6)
+        sizer.AddMany([PasswordText,button])
+        panel.SetSizer(sizer)
+        self.SetSize((200, 65))
+        self.Centre()
+        self.Show()
+    #def updateTitle(self, title):
+    #    self.SetTitle(title)
+    def OnQuit(self, e):
+        self.Close()
 
 
 class LoginWindow(wx.Frame):
@@ -53,11 +83,15 @@ class LoginWindow(wx.Frame):
             login = LoginText.GetLineText(0)
             password = PasswordText.GetLineText(0)
             token = TokenText.GetLineText(0)
-            print(login, password, token)
             statusbar.SetStatusText("Trying to login...")
-
+            asktext = AskWindow(None,style=wx.CAPTION | wx.CLIP_CHILDREN)
+            asktext.SetTitle(locale['2fa'])
+            self.Disable()
+            twoth = asktext.code
+            print(login,token,password,twoth)
         def gettoken(self):
             webbrowser.open(token_page)
+
         gettokenButton = wx.Button(panel, wx.ID_ANY, locale['gettoken'], (10, 10))
         gettokenButton.Bind(wx.EVT_BUTTON, gettoken)
         button = wx.Button(panel, wx.ID_ANY, locale['login'], (10, 10))
@@ -69,13 +103,15 @@ class LoginWindow(wx.Frame):
                         TokenLabel, TokenText,
                         button, gettokenButton])
         panel.SetSizer(sizer) 
-        self.Fit()
         self.SetSize((280, 180))
         self.SetTitle("wxVk "+locale['login'])
         self.Centre()
-
+    
     def OnQuit(self, e):
         self.Close()
+    def reenable(self):
+        self.Enable()
+        print("done")
 
 
 class MainWindow(wx.Frame):
